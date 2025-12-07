1639facb9129057a899a950250dbb1cf1e139a8a
royalMacOs <royalclubcanada@gmail.com>
2025-12-07 12:57:43 -0500

    Initial Commit

---
+++ royalbadmintonclub/royalbadmintonclubApp.swift
@@ -0,0 +1,17 @@
+//
+//  royalbadmintonclubApp.swift
+//  royalbadmintonclub
+//
+//  Created by Royal Badminton on 2025-12-07.
+//
+
+import SwiftUI
+
+@main
+struct royalbadmintonclubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
